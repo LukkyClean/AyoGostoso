1150e2b2cd90a2556abdf9e408171fe3c515ba18
LukkyClean <147477857+LukkyClean@users.noreply.github.com>
2025-06-23 23:57:23 -0300

    Delete BusyBus directory

--- BusyBus/controllers/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    require_once __DIR__ . '/../models/Model.php';
-
-    header('Content-Type: application/json');
-    $input = json_decode(file_get_contents("php://input"), true);
-
-    $model = new Model();
-
-?>
